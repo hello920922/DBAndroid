0a0bdc094351baf320cb05e2ee182d187e3e1445
ansqja91 <ansqja91@naver.com>
2015-11-25 03:33:13 +0900

    - Daniel_Beom -

--- app/src/main/java/hongik/android/project/best/MainActivity.java
+++ app/src/main/java/hongik/android/project/best/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review);
+        setContentView(R.layout.activity_history);
 
         backHandler = new BackPressCloseHandler(this);
     }
