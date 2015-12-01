af0d9a5edf25e79310278a6d591481147ecb1b62
hello920922 <hello920922@gmail.com>
2015-12-01 23:13:26 +0900

    Change ServerURL (MySQL -> Oracle)

--- app/src/main/java/hongik/android/project/best/Constant.java
+++ app/src/main/java/hongik/android/project/best/Constant.java
@@ -4,8 +4,8 @@ package hongik.android.project.best;
  * Created by Mingyu Park on 2015-11-23.
  */
 public class Constant {;
-    //public static String ServerURL = "http://203.249.87.220/db_project/501_team3/BeSt/function/";
-    public static String ServerURL = "http://hello0922.iptime.org/Database/function/";
+    public static String ServerURL = "http://203.249.87.220/db_project/501_team3/BeSt/function/";
+    //public static String ServerURL = "http://hello0922.iptime.org/Database/function/";
     public static String QueryURL = ServerURL + "android.php";
     public static String IMAGESERVER = ServerURL + "uploads/";
 }
